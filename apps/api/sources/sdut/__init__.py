f75622dc3239f7c63634c94b70bc004d4414567e
MeiK <meik2333@gmail.com>
2026-03-28 19:16:29 +0800

    fix: sdutacm url

--- apps/api/sources/sdut/__init__.py
+++ apps/api/sources/sdut/__init__.py
@@ -17,13 +17,13 @@ class SDUT:
     async def login(username: str, password: str):
         async with httpx.AsyncClient() as client:
             session_url = (
-                "https://acm.sdut.edu.cn/onlinejudge3/api/getSession?t="
+                "https://oj.sdutacm.cn/onlinejudge3/api/getSession?t="
                 + str(time.time() * 1000)
             )
             resp = await client.get(session_url)
             csrf = resp.cookies.get("csrfToken")
             headers = {"x-csrf-token": csrf}
-            login_url = "https://acm.sdut.edu.cn/onlinejudge3/api/login"
+            login_url = "https://oj.sdutacm.cn/onlinejudge3/api/login"
             resp = await client.post(
                 login_url,
                 json={"loginName": username, "password": password},
